c9f0d4887c25795558cb37efac20fe7dc688a48f
Arzel John Zolina <Arzeljrz17@gmail.com>
2023-12-14 22:23:11 +0800

    config database

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli('localhost', 'root', 'arzelzolina10', 'db_ls') or die(mysqli_error());
+$conn = new mysqli('localhost', 'root', '', 'db_ls') or die(mysqli_error());
 if (!$conn) {
 	die("Fatal Error: Connection Failed!");
 }
